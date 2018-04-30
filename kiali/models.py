753c9f4cbc7af61ccf30fee7b02376d6ea6bb5cc
Guilherme Baufaker Rêgo <gbaufake@redhat.com>
2018-04-30 09:31:27 -0300

    Updating to 0.3.7

--- kiali/models.py
+++ kiali/models.py
@@ -3,7 +3,7 @@ from kiali.client import ApiObject
 class Data (ApiObject):
     __slots__ = [
         'id', "source", 'target', 'version', 'text', 'color', 'style', 'rate', 'service', 'group_by',
-        "is_root", "has_CB"
+        "is_root", "has_c_b", "parent"
     ]
 
     def __repr__(self):
