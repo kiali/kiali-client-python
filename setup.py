cdd42904aa719c31a5f6ba768f77d5fa2556a363
Guilherme Baufaker Rêgo <gbaufake@redhat.com>
2018-09-27 16:14:17 -0300

    Releasing Kiali Client 0.4.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kiali-client",
-    version="0.4.8",
+    version="0.4.9",
     author="Guilherme Baufaker Rego",
     author_email="gbaufake@redhat.com",
     description="Python client to communicate with Kiali server over HTTP(S)",
