b691b3cb3a80d4f83bc30d7de9e0f43c42a25743
Hervé Beraud <hberaud@redhat.com>
2019-01-07 18:54:42 +0100

    Prefer to use PBR instead of setuptools
    Gains:
    - manage versions/releases only by creating tags
    - centralize setup config into setup.cfg
    - remove configuration from python file
    - generate ChangeLog automatically
    - generate AUTHORS file automatically
    - manage pypi version automatically with git tag and travis-ci

--- setup.py
+++ setup.py
@@ -1,25 +1,7 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
-    name="kiali-client",
-    version="0.6.1",
-    author="Guilherme Baufaker Rego",
-    author_email="gbaufake@redhat.com",
-    description="Python client to communicate with Kiali server over HTTP(S)",
-    long_description=long_description,
+    requires=['pbr'],
+    pbr=True,
     long_description_content_type="text/markdown",
-    url="https://github.com/kiali/kiali-client-python",
-    license='Apache License 2.0',
-    keywords = "kiali, service-mesh, istio, kurbenetes, openshift",
-    packages=setuptools.find_packages(),
-    classifiers=(
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Developers',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Topic :: System :: Monitoring',
-    ),
 )
